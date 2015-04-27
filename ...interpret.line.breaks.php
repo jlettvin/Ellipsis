243eca83cbdc4a169de6d59a4bff4482d3e9108d
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-04-27 08:16:04 -0400

    Improved example paper

--- ...interpret.line.breaks.php
+++ ...interpret.line.breaks.php
@@ -1,8 +1,19 @@
 <?php
 function interpret_line_breaks($line) {
-    $line = str_replace("^^^^", '<br clear="all" /><br />', $line);
-    $line = str_replace("^^^", '<br />', $line);
-    $line = str_replace("-----", '<hr />', $line);
+    $line = str_replace(
+        "^^^^^",
+        '<br clear="all" /><br />'.
+        '<div style="page-break-after:always"></div>',
+        $line);
+    $line = str_replace(
+        "^^^^",
+        '<br clear="all" /><br />', $line);
+    $line = str_replace(
+        "^^^",
+        '<br />', $line);
+    $line = str_replace(
+        "-----",
+        '<hr />', $line);
     return $line;
 }
 ?>
